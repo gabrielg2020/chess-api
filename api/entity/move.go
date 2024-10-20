12337fe87319086f403f566260e3cab02aebb581
Gabriel Guimaraes <80750139+gabrielg2020@users.noreply.github.com>
2024-10-21 00:45:42 +0100

    feat: add move getter functions and interface
    Signed-off-by: Gabriel Guimaraes <80750139+gabrielg2020@users.noreply.github.com>

--- api/entity/move.go
+++ api/entity/move.go
@@ -1,5 +1,17 @@
 package entity
-type Move struct {
+
+type MoveEntityInterface interface {
+	GetFromX() (int, error)
+	GetFromY() (int, error)
+	GetToX() (int, error)
+	GetToY() (int, error)
+	GetPromotion() (int, error)
+	IsCastling() (bool, error)
+	IsEnPassant() (bool, error)
+	GetCaptured() (int, error)
+}
+
+type MoveEntity struct {
 	fromX       int
 	fromY       int
 	toX         int
@@ -8,4 +20,38 @@ type Move struct {
 	isCastling  bool
 	isEnPassant bool
 	captured    int // 0 if no piece was captured, otherwise hold the piece code of the piece captured
+}
+
+// TODO Create getter functions for all methods
+
+func (entity *MoveEntity) GetFromX() (int, error) {
+	return entity.fromX, nil
+}
+
+func (entity *MoveEntity) GetFromY() (int, error) {
+	return entity.fromY, nil
+}
+
+func (entity *MoveEntity) GetToX() (int, error) {
+	return entity.toX, nil
+}
+
+func (entity *MoveEntity) GetToY() (int, error) {
+	return entity.toY, nil
+}
+
+func (entity *MoveEntity) GetPromotion() (int, error) {
+	return entity.promotion, nil
+}
+
+func (entity *MoveEntity) IsCastling() (bool, error) {
+	return entity.isCastling, nil
+}
+
+func (entity *MoveEntity) IsEnPassant() (bool, error) {
+	return entity.isEnPassant, nil
+}
+
+func (entity *MoveEntity) GetCaptured() (int, error) {
+  return entity.captured, nil
 }
