5b84aaf1dcaa5490a4bb7646f6e8e4d0c3ed0277
Gabriel Guimaraes <80750139+gabrielg2020@users.noreply.github.com>
2024-10-24 20:50:48 +0100

    feat: add boilerplate for getting a piece move
    Signed-off-by: Gabriel Guimaraes <80750139+gabrielg2020@users.noreply.github.com>

--- api/service/move_service/move_service.go
+++ api/service/move_service/move_service.go
@@ -2,6 +2,7 @@ package MoveService
 
 import (
 	"errors"
+	"math"
 
 	"github.com/gabrielg2020/chess-api/api/entity"
 )
@@ -31,6 +32,20 @@ func (service *MoveService) FindBestMove(chessboard entity.ChessboardEntityInter
 			if piece == 0 {
 				continue
 			}
+			switch math.Abs(float64(piece)) {
+			case 1: // Get Pawn Move
+				getPawnMove(piece, row, col)
+			case 2: // Get Knight Move
+				getKnightMove(piece, row, col)
+			case 3: // Get Bishop Move
+				getBishopMove(piece, row, col)
+			case 4: // Get Rook Move
+				getRookMove(piece, row, col)
+			case 5: // Get Queen Move
+				getQueenMove(piece, row, col)
+			case 6: // Get King Move
+				getKingMove(piece, row, col)
+			}
 		}
 	}
 		// c. For each piece, find all moves
@@ -40,4 +55,121 @@ func (service *MoveService) FindBestMove(chessboard entity.ChessboardEntityInter
 	// 3. Return Legal Moves
 		// a. Return moves array
 	return moves, nil
+}
+func getPawnMove (piece int, fromX int, fromY int) ([]entity.MoveEntityInterface, error) {
+
+	toX, toY, promotion, isCastling, isEnPassant, captured := 1, 1, 1, true, true, 1
+	move := entity.NewMoveEntity(
+		&fromX, 
+		&fromY, 
+		&toX, 
+		&toY, 
+		&promotion, 
+		&isCastling,
+		&isEnPassant, 
+		&captured,
+	)
+	var moves []entity.MoveEntityInterface
+	moves = append(moves, move)
+	return moves, nil
+}
+
+func getKnightMove (piece int, fromX int, fromY int) ([]entity.MoveEntityInterface, error) {
+	toX, toY, promotion, isCastling, isEnPassant, captured := 1, 1, 1, true, true, 1
+	move := entity.NewMoveEntity(
+		&fromX, 
+		&fromY, 
+		&toX, 
+		&toY, 
+		&promotion, 
+		&isCastling,
+		&isEnPassant, 
+		&captured,
+	)
+	var moves []entity.MoveEntityInterface
+	moves = append(moves, move)
+	return moves, nil
+}
+
+func getBishopMove (piece int, fromX int, fromY int) ([]entity.MoveEntityInterface, error) {
+	toX, toY, promotion, isCastling, isEnPassant, captured := 1, 1, 1, true, true, 1
+	move := entity.NewMoveEntity(
+		&fromX, 
+		&fromY, 
+		&toX, 
+		&toY, 
+		&promotion, 
+		&isCastling,
+		&isEnPassant, 
+		&captured,
+	)
+	var moves []entity.MoveEntityInterface
+	moves = append(moves, move)
+	return moves, nil
+}
+
+func getRookMove (piece int, fromX int, fromY int) ([]entity.MoveEntityInterface, error) {
+	toX, toY, promotion, isCastling, isEnPassant, captured := 1, 1, 1, true, true, 1
+	move := entity.NewMoveEntity(
+		&fromX, 
+		&fromY, 
+		&toX, 
+		&toY, 
+		&promotion, 
+		&isCastling,
+		&isEnPassant, 
+		&captured,
+	)
+	var moves []entity.MoveEntityInterface
+	moves = append(moves, move)
+	return moves, nil
+}
+
+func getQueenMove (piece int, fromX int, fromY int) ([]entity.MoveEntityInterface, error) {
+	toX, toY, promotion, isCastling, isEnPassant, captured := 1, 1, 1, true, true, 1
+	move := entity.NewMoveEntity(
+		&fromX, 
+		&fromY, 
+		&toX, 
+		&toY, 
+		&promotion, 
+		&isCastling,
+		&isEnPassant, 
+		&captured,
+	)
+	var moves []entity.MoveEntityInterface
+	moves = append(moves, move)
+	return moves, nil
+}
+
+func getKingMove (piece int, fromX int, fromY int) ([]entity.MoveEntityInterface, error) {
+	toX, toY, promotion, isCastling, isEnPassant, captured := 1, 1, 1, true, true, 1
+	move := entity.NewMoveEntity(
+		&fromX, 
+		&fromY, 
+		&toX, 
+		&toY, 
+		&promotion, 
+		&isCastling,
+		&isEnPassant, 
+		&captured,
+	)
+	var moves []entity.MoveEntityInterface
+	moves = append(moves, move)
+	return moves, nil
+}
+
+func isMoveOutOfBounds (move entity.MoveEntityInterface) (error) {
+	toX, errX := move.GetToX()
+	toY, errY := move.GetToY()
+
+	if (errX != nil) || (errY != nil) {
+		return errors.New("failed to retrive toX or toY")
+	}
+
+	if (toX > 7) || (toX < 0) || (toY > 7) || (toY < 0) {
+		return errors.New("move out of bounds")
+	} else {
+		return nil
+	}
 }
