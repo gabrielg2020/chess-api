66d5c2a8e898599856aa116eb04b86f30b20e722
Gabriel Guimaraes <80750139+gabrielg2020@users.noreply.github.com>
2024-11-02 22:47:58 +0000

    fix: track logger.go

---
+++ pkg/logger/logger.go
@@ -0,0 +1,26 @@
+package logger
+
+import (
+	"os"
+
+	"github.com/sirupsen/logrus"
+)
+
+// Log Global logger
+var Log *logrus.Logger
+
+func init() {
+	Log = logrus.New()
+
+	Log.SetOutput(os.Stdout)
+
+	Log.SetFormatter(&logrus.TextFormatter{
+		TimestampFormat: "2006-01-02 15:04:05",
+		FieldMap: logrus.FieldMap{
+			logrus.FieldKeyTime:  "@timestamp",
+			logrus.FieldKeyLevel: "@level",
+			logrus.FieldKeyMsg:   "@message",
+		},
+	})
+	Log.SetLevel(logrus.InfoLevel)
+}
