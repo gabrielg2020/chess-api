63389e031296f306152d6fa64e374cbc39660a23
Gabriel Guimaraes <80750139+gabrielg2020@users.noreply.github.com>
2024-11-05 19:09:01 +0000

    refactor: make `chessboard.isWithinBounds` global

--- api/entity/chessboard.go
+++ api/entity/chessboard.go
@@ -118,7 +118,7 @@ func (entity *ChessboardEntity) GetFullmoveNumber() (string, error) {
 }
 
 func (entity *ChessboardEntity) GetPiece(row int, col int) (int, error) {
-	if !entity.isWithinBounds(row, col) {
+	if !entity.IsWithinBounds(row, col) {
 		return -7, errors.New("ChessboardEntity.GetPiece: row or col out of bounds")
 	}
 
@@ -130,7 +130,7 @@ func (entity *ChessboardEntity) GetPiece(row int, col int) (int, error) {
 }
 
 func (entity *ChessboardEntity) IsSquareEmpty(row int, col int) (bool, error) {
-	if !entity.isWithinBounds(row, col) {
+	if !entity.IsWithinBounds(row, col) {
 		return false, nil
 	}
 
@@ -145,7 +145,7 @@ func (entity *ChessboardEntity) IsSquareEmpty(row int, col int) (bool, error) {
 }
 
 func (entity *ChessboardEntity) IsOpponent(piece int, row int, col int) (bool, error) {
-	if !entity.isWithinBounds(row, col) {
+	if !entity.IsWithinBounds(row, col) {
 		return false, nil
 	}
 
@@ -186,7 +186,7 @@ func (entity *ChessboardEntity) IsOpponent(piece int, row int, col int) (bool, e
 	return true, nil
 }
 
-func (entity *ChessboardEntity) isWithinBounds(toX int, toY int) bool {
+func (entity *ChessboardEntity) IsWithinBounds(toX int, toY int) bool {
 	if (toX > 7) || (toX < 0) || (toY > 7) || (toY < 0) {
 		return false
 	} else {
