08cd43b849662adad356af9b43848c47f570b594
Gabriel Guimaraes <80750139+gabrielg2020@users.noreply.github.com>
2024-11-06 19:44:30 +0000

    refactor: move psudeo-move functionality into `MoveService.FindPseudoLegalMoves`
    Signed-off-by: Gabriel Guimaraes <80750139+gabrielg2020@users.noreply.github.com>

--- api/mocks/move_service_mock.go
+++ api/mocks/move_service_mock.go
@@ -13,3 +13,8 @@ func (m *MockMoveService) FindBestMove(chessboard entity.ChessboardEntityInterfa
 	args := m.Called(chessboard)
 	return args.Get(0).(entity.MoveEntityInterface), args.Error(1)
 }
+
+func (m *MockMoveService) FindPseudoLegalMoves(colour string, chessboard entity.ChessboardEntityInterface) ([]entity.MoveEntityInterface, error) {
+	args := m.Called(colour, chessboard)
+	return args.Get(0).([]entity.MoveEntityInterface), args.Error(1)
+}
